bd0cde5c518f86914f8f76cb1a386cad620c9d46
Alex Machado <Alexmachado2045@gmail.com>
2025-10-18 23:53:35 -0300

    Correção da tela3 + Sem tarefas agregado

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router";
 
 function Tasks(props) {
   const navigate = useNavigate();
+  const tasks = props.tasks || [];
 
   // Função para navegar para a página de detalhes da tarefa
   function onSeeDetailsClick(task) {
@@ -15,37 +16,41 @@ function Tasks(props) {
 
   return (
     <div>
-      <ul className=" space-y-2 p-6 bg-slate-700 rounded-md shadow">
-        {props.tasks.map((task) => (
-          <li key={task.id} className="flex space-x-4">
-            <button // Botão de marcar tarefa como completa ou incompleta
-              onClick={() => props.onTaskClick(task.id)}
-              className={`font-bold text-xl bg-slate-600 w-full p-2 rounded-md ${
-                task.isCompleted
-                  ? " text-slate-400 line-through"
-                  : "text-yellow-300"
-              }`}
-            >
-              {task.title}
-            </button>
+      {tasks.length === 0 ? (
+        <p className="p-6 text-slate-300">Sem tarefas</p>
+      ) : (
+        //Se tasks estiver vazio → mostra "<p>Sem tarefas</p> Se tasks tiver algum item → mostrar a lista de tarefas
+        <ul className=" space-y-2 p-6 bg-slate-700 rounded-md shadow">
+          {tasks.map((task) => (
+            <li key={task.id} className="flex space-x-4">
+              <button // Botão de marcar tarefa como completa ou incompleta
+                onClick={() => props.onTaskClick(task.id)}
+                className={`font-bold text-xl bg-slate-600 w-full p-2 rounded-md ${
+                  task.isCompleted
+                    ? " text-slate-400 line-through"
+                    : "text-yellow-300"
+                }`}
+              >
+                {task.title}
+              </button>
 
-            <button // Botão de ver detalhes da tarefa
-              onClick={() => onSeeDetailsClick(task)}
-              className="bg-yellow-300 rounded-md p-3"
-            >
-              <ChevronRight />
-            </button>
+              <button // Botão de ver detalhes da tarefa
+                onClick={() => onSeeDetailsClick(task)}
+                className="bg-yellow-300 rounded-md p-3"
+              >
+                <ChevronRight />
+              </button>
 
-            <button // Botão de deletar tarefa
-              onClick={() => props.onDeleteTaskClick(task.id)}
-              className="bg-yellow-300 rounded-md p-3 hover:bg-red-500"
-            >
-              <Trash />
-            </button>
-          </li>
-        ))}
-      </ul>
-      */ /*
+              <button // Botão de deletar tarefa
+                onClick={() => props.onDeleteTaskClick(task.id)}
+                className="bg-yellow-300 rounded-md p-3 hover:bg-red-500"
+              >
+                <Trash />
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
